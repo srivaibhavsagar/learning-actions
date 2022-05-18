e600e72758daf9782f721d33d2b7f29ad65fc7f9
“Vaibhav <vaibhavsrivastava59@gmail.com>
2022-05-18 01:47:06 +0100

    dep1234

--- start.sh
+++ start.sh
@@ -1,5 +1,26 @@
 git clone https://github.com/srivaibhavsagar/terraform--poc.git
+
 cd terraform--poc/Services/main
+
+rm -rf backend.tf
+
+USAGE=$(cat <<-END
+    terraform {
+  required_version = ">= 0.12"
+  backend "s3" {
+    key = "module-state/${1}/terraform.tfstate"
+  }
+}
+END
+)
+
+echo $USAGE >> backend.tf
+
+
 terraform init --backend-config='../../template/backend_config.hcl'
 terraform plan
 terraform apply --auto-approve
+
+
+
+
